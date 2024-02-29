0d6088adb9875b31b4436f263e79442e03211972
Alan-Britoo <rotcarrillo_8@hotmail.com>
2024-02-29 00:29:15 -0500

    GG

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import {Button, ButtonGroup} from "@nextui-org/react";
+import './App.css'
+import Appp from "./Componets/tabla";
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Button color="warning">
+        Warning
+      </Button> 
+      <Appp/>
+     
+    </>
+  )
+}
+
+export default App
